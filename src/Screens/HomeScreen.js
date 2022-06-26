e2b361b4ebfd3051ba7e52c5070962af524ab859
Cayaf <corentin.fayac@orange.fr>
2022-06-26 10:14:08 +0200

    First react native commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function HomeScreen() {
+    return (
+        <View
+            style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
